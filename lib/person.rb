4163996f083de7c9d3515c187b9b7f0c9e89a94f
github username <github email address>
2019-06-26 15:33:21 -0400

    happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
 # your code goes here
+class Person
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+    def name 
+        @name
+    end
+
+    def bank_account
+        @bank_account
+    end
+    def bank_account=(new_balance)
+        @bank_account = new_balance
+    end
+    def happiness
+        @happiness
+    end 
+    def happiness=(new_happiness)
+        if new_happiness <= 10
+            @happiness = new_happiness
+        end 
+    end
+
+end
